e58b179bac2c9d621bdaf5508c9a5243703900ae
AzrielTheHellrazor <goktug200402@gmail.com>
2025-10-03 11:42:46 +0300

    add ownable to contract

--- contracts/src/ImageGenerator.sol
+++ contracts/src/ImageGenerator.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IERC20 {
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
 }
 
-contract ImageGenerator {
+contract ImageGenerator is Ownable {
     IERC20 public immutable usdc;
     uint256 public constant PRICE = 1e6; // 1 USDC (6 decimals)
     
-    address public owner;
-    
     event PaymentReceived(address indexed user, uint256 amount);
+    event Withdraw(address indexed owner, uint256 amount);
     
-    constructor(address _usdc) {
+    constructor(address _usdc) Ownable(msg.sender) {
         usdc = IERC20(_usdc);
-        owner = msg.sender;
     }
     
     function payForImages() external {
@@ -23,5 +25,18 @@ contract ImageGenerator {
         require(usdc.transferFrom(msg.sender, address(this), PRICE), "USDC transfer failed");
         
         emit PaymentReceived(msg.sender, PRICE);
+    }
+    
+    /**
+     * @dev Withdraw function that can only be called by the owner
+     * Transfers all USDC tokens in the contract to the owner
+     */
+    function withdraw() external onlyOwner {
+        uint256 balance = usdc.balanceOf(address(this));
+        require(balance > 0, "No tokens to withdraw");
+        
+        require(usdc.transfer(owner(), balance), "Transfer failed");
+        
+        emit Withdraw(owner(), balance);
     }
 }
